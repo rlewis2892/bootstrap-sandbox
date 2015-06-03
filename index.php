bb380109f4ae161882788ea0799777158b1cd9a4
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-06-03 14:35:32 -0600

    create lorchard feature branch, upload content

--- index.php
+++ index.php
@@ -1,33 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!--this brings in all head links-->
 		<?php require_once("lib/head-utils.html"); ?>
-		<title>Bootstrap Sandbox</title>
+		<title>Lorchard</title>
 	</head>
-	<body class="site">
+	<body class="bg-sky site">
 
 		<!--header include-->
 		<?php require_once("lib/header.html"); ?>
 
+		<!--all content lives in here-->
 		<main class="site-content">
+
 			<div class="container">
 
-				<!-- **************** -->
-				<!--Start Example Here-->
-				<!-- **************** -->
-				<p>Examine the source code on this page for the sticky footer and jQuery slideToggle.</p>
+				<!--welcome message section-->
+				<section class="welcome">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="col-md-8 col-md-offset-2">
+								<h2>Great Big Welcome Message!</h2>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="col-md-8 col-md-offset-2">
+								<p>Check out our magical product you want to purchase very much!</p>
+							</div>
+						</div>
+					</div>
+				</section>
 
-				<!--This is the clickable question div-->
-				<div class="qa-panel" id="question">
-					<p>Q: Is there an easy way to do a jQuery slideToggle panel? What the heck? Click me and find out...</p>
-				</div>
+				<hr/>
 
-				<!--This is the answer toggle-->
-				<div class="qa-panel" id="answer">
-					<p>A: YES! There is!!! :D Just take a look at the code...</p>
-				</div>
+				<!--content section-->
+				<section>
+					<!--begin 1/3 column content layout-->
+					<div class="row">
+						<div class="col-md-4">
+							<p>This is a 1/3 width column of content. There is a button below.</p>
+							<a class="btn btn-info" href="#">Button</a>
+						</div>
+						<div class="col-md-4">
+							<p>This is a 1/3 width column of content. There is a button below.</p>
+							<a class="btn btn-info" href="#">Button</a>
+						</div>
+						<div class="col-md-4">
+							<p>This is a 1/3 width column of content. There is a button below.</p>
+							<a class="btn btn-info" href="#">Button</a>
+						</div>
+					</div><!--.row-->
 
-			</div>
+					<hr/>
+
+					<!--begin full-width content layout-->
+					<div class="row">
+						<div class="col-xs-12">
+							<p>Examine the source code for the jQuery slideToggle.</p>
+
+							<!--This is the clickable question div-->
+							<div class="qa-panel" id="question">
+								<p>Q: Is there an easy way to do a jQuery slideToggle panel? What the heck? Click me and find out...</p>
+							</div>
+
+							<!--This is the answer toggle-->
+							<div class="qa-panel" id="answer">
+								<p>A: YES! There is!!! :D Just take a look at the code...</p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			</div><!--.container-->
 		</main>
 
 		<!--footer include-->
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+	/*jQuery fadeIn*/
+	$('main').hide().fadeIn(2000);
+
+	/*jQuery slideToggle*/
 	$("#question").click(function(){
 		$("#answer").slideToggle("slow");
 	});
