a1888e232227a5d2fb56e2f76f0e7d03f55c8626
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-06-08 10:34:55 -0600

    edit Frood logged-in layout

--- index.php
+++ index.php
@@ -4,117 +4,68 @@
 		<?php require_once("lib/head-utils.html"); ?>
 		<title>Bootstrap Sandbox</title>
 	</head>
-	<body>
+	<body class="site">
 
 		<!--header include-->
 		<?php require_once("lib/header.html"); ?>
 
-		<main>
-			<div class="container">
-				<p>The above example is a navbar with a login form inside a dropdown button. See HTML and CSS code below :-)</p>
-				<h3>HTML:</h3>
-				<pre>
-&lt;header&gt;
-  &lt;div class="container"&gt;
-    &lt;nav class="navbar"&gt;
+		<main class="site-content">
+			<div class="container-fluid">
 
-      &lt;!-- logo and mobile toggle button get grouped together for better mobile display --&gt;
-      &lt;div class="navbar-header"&gt;
+				<!--I put the welcome message on top - full width-->
+				<div class="row welcome-msg">
+					<div class="col-xs-12">
+						<h2>Welcome!</h2>
+						<p>Use the dropdown on the left side of the page to begin your search.</p>
+					</div>
+				</div>
 
-        &lt;!-- this is the mobile menu button --&gt;
-        &lt;button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu"&gt;
-          &lt;span class="sr-only"&gt;main menu&lt;/span&gt;
-          &lt;span class="glyphicon glyphicon-menu-hamburger"&gt;&lt;/span&gt;
-        &lt;/button&gt;
+				<!--I created another row to house the search dropdown, search forms, and output -->
+				<div class="row">
+					<div class="col-md-3 side-panel-column">
+						<?php require_once("lib/side-panel.php"); ?>
+					</div>
+					<div class="col-md-9">
 
-        &lt;a class="navbar-brand" href="#"&gt;Froodies!&lt;/a&gt;
-      &lt;/div&gt;
+						<div class="search-form-container col-md-6">
+							<!-- START PEOPLE SEARCH -->
+							<div id="people-search" class="searchForm">
+								<?php require_once("lib/views/people-searchform.php"); ?>
+							</div>
+							<!--	END PEOPLE SEARCH -->
 
-      &lt;!-- here are your main nav links, grouped for toggling --&gt;
-      &lt;div class="collapse navbar-collapse" id="main-menu"&gt;
-        &lt;ul class="nav navbar-nav navbar-right"&gt;
-          &lt;li&gt;&lt;a href="#"&gt;Home&lt;/a&gt;&lt;/li&gt;
-          &lt;li&gt;&lt;a href="#"&gt;Link1&lt;/a&gt;&lt;/li&gt;
-          &lt;li&gt;&lt;a href="#"&gt;Link2&lt;/a&gt;&lt;/li&gt;
-          &lt;li&gt;&lt;a href="#"&gt;Link3&lt;/a&gt;&lt;/li&gt;
-          &lt;li&gt;&lt;a href="#" class="nav-button btn btn-default"&gt;Sign Up&lt;/a&gt;&lt;/li&gt;
-          &lt;li class="dropdown"&gt;
-            &lt;button type="button" class="nav-button dropdown-toggle btn btn-default" data-toggle="dropdown" role="button" aria-expanded="false"&gt;Sign In&nbsp;&nbsp;&lt;span class="caret"&gt;&lt;/span&gt;&lt;/button&gt;
+							<!-- START RESTAURANT SEARCH -->
+							<div id="restaurant-search" class="searchForm">
+								<?php require_once("lib/views/restaurant-search-view.php"); ?>
+							</div>
+							<!-- END RESTAURANT SEARCH -->
 
-            &lt;div class="dropdown-menu"&gt;
-              &lt;p&gt;Sign into your account:&lt;/p&gt;
-              &lt;form class="navbar-form"&gt;
-                &lt;div class="form-group"&gt;
-                &lt;input id="txtUsername001" type="text" class="form-control" placeholder="username"&gt;
-                  &lt;input id="pswPassword001" type="password" class="form-control" placeholder="password"&gt;
-                &lt;/div&gt;
-                &lt;button type="submit" class="btn btn-default pull-right"&gt;Submit&lt;/button&gt;
-                &lt;div class="clearfix"&gt;&lt;/div&gt;
-              &lt;/form&gt;
-            &lt;/div&gt;
+							<!-- START LOCATION SEARCH -->
+							<div id="location-search" class="searchForm">
+								<?php require_once("lib/views/locationsearch-view.php"); ?>
+							</div>
+							<!-- END LOCATION SEARCH -->
 
-          &lt;/li&gt;
+							<!-- START PREFERENCES SEARCH -->
+							<div id="preference-search" class="searchForm">
+								<?php require_once("lib/views/preference-search-form.php"); ?>
+							</div>
+							<!-- END PREFERENCES SEARCH -->
+						</div>
 
-        &lt;/ul&gt;
-      &lt;/div&gt;
-    &lt;/nav&gt;
-  &lt;/div&gt;
-&lt;/header&gt;</pre>
-				<h3>CSS:</h3>
-				<pre>
-/********************************************
-* Base Styles
-********************************************/
-header {
-  padding: 20px 0;
-  border-bottom: 1px solid #ddd;
-}
+						<!-- SEARCH RESULTS OUTPUT AREA FOR ALL SEARCH TYPES -->
+						<div class="col-xs-12" id="outputArea">
+							test test test test test test the output area
+						</div>
 
-main {
-  margin-top: 40px;
-}
+					</div>
+				</div>
 
-/********************************************
-* Navbar Styles
-********************************************/
-.navbar {
-  margin: 0;
-}
-
-.navbar-brand {
-  font-size: 2em;
-}
-
-button.nav-button {
-  margin: 8px 0 8px 10px;
-}
-
-.navbar-toggle {
-  border: 1px solid #ddd;
-  padding: 10px 15px;
-  margin: 0;
-}
-
-/********************************************
-* Form Styles
-********************************************/
-.dropdown-menu p {
-  padding: 10px 15px 0;
-  margin: 0;
-}
-
-.navbar-form input {
-  margin-bottom: 5px;
-}
-
-@media only screen and (max-width: 768px) {
-  .navbar-form {margin: 0;}
-}</pre>
 			</div>
 		</main>
 
 		<!--footer include-->
-		<?php /*require_once("lib/footer.html"); */?>
+		<?php require_once("lib/footer.html"); ?>
 
 	</body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
+	$("#search-selector").change(function() {
+		var selector=$("#search-selector").val();
+		$(".searchForm").hide();
+		$("#" +selector).show();
+
+		// load the user's location if location was selected
+		/*if(selector === "location-search") {
+			geoFindMe();
+		}*/
+	});
 
 });
