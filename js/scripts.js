a3ba933a4b4193bbe575dc1321191832516c9daf
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-06-09 08:36:54 -0600

    edit fade in

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
 	/*jQuery fadeIn*/
-	$('main').hide().fadeIn(2000);
+	/*$('main').hide().fadeIn(2000)*/;
+	$('main, header, footer').hide().fadeIn(3000);
 
 	/*jQuery slideToggle*/
 	$("#question").click(function(){
