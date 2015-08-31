56121c8c8fb46a1acff11c707085f8a074e1d9f1
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-08-31 10:03:58 -0600

    style GSE

--- index.php
+++ index.php
@@ -26,6 +26,24 @@ require_once("lib/template/head-utils.php");
 		<hr>
 		<!--i created a page with all the product items here-->
 		<?php require_once("lib/template/products.php"); ?>
+
+		<!--google custom search here-->
+		<h3>Google Custom Search</h3>
+		<div class="gse-wrap">
+			<script>
+				(function() {
+					var cx = '009358953223849865007:rd-syvhf6vw';
+					var gcse = document.createElement('script');
+					gcse.type = 'text/javascript';
+					gcse.async = true;
+					gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+						'//cse.google.com/cse.js?cx=' + cx;
+					var s = document.getElementsByTagName('script')[0];
+					s.parentNode.insertBefore(gcse, s);
+				})();
+			</script>
+			<gcse:search></gcse:search>
+		</div>
 	</div><!--.container-->
 </div><!--.site-content-->
 
