c8ccac8276a58af8f05a3d188abce0506a2d73d0
Rochelle Lewis <rlewis37@cnm.edu>
2016-03-12 16:23:01 -0700

    add images, update example code

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 							<p>This example is set to work on the <strong>md</strong> Bootstrap breakpoint on up. Adjust the
 								<code>min-width</code> on the @media query if desired.
 								<br><strong>View on GitHub</strong>: <a
-									href="https://github.com/rlewis2892/bootstrap-sandbox/tree/flexbox-columns" target="_blank">github.com/rlewis2892/bootstrap-sandbox/tree/flexbox-columns</a>
+									href="https://github.com/rlewis2892/bootstrap-sandbox/tree/hackyhooker" target="_blank">https://github.com/rlewis2892/bootstrap-sandbox/tree/hackyhooker</a>
 							</p>
 						</div>
 					</div>
@@ -189,31 +189,76 @@
 				<pre>
 &lt;!-- Begin Columns Here --&gt;
 &lt;div class="row row-flex"&gt;
-  &lt;div class="col-md-3"&gt;
-    &lt;div class="content-box"&gt;
-      &lt;h4&gt;Riker Ipsum&lt;/h4&gt;
-      &lt;p&gt;Travel time to the nearest starbase? Fear is the true enemy, the only enemy. Now we know what they mean by 'advanced' tactical training. Flair is what marks the difference between artistry and mere competence. Yes, absolutely, I do indeed concur, wholeheartedly! But the probability of making a six is no greater than that of rolling a seven. Some days you get the bear, and some days the bear gets you. We finished our first sensor sweep of the neutral zone. I've had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Not if I weaken first. I guess it's better to be lucky than good.&lt;/p&gt;
+
+  &lt;div class="col-sm-6 col-md-4"&gt;
+    &lt;div class="thumbnail"&gt;
+      &lt;img src="images/icon-ddcb-lg.png" alt="Deep Dive Logo" height="60" width="100"&gt;
+      &lt;div class="caption"&gt;
+        &lt;h3&gt;CNM STEMulus Center Deep Dive Coders Bootcamp&lt;/h3&gt;
+        &lt;h3&gt;Albuquerque, NM&lt;/h3&gt;
+        &lt;h4&gt;Capstone Project:&lt;/h4&gt;
+        &lt;p&gt;Project: Co-developer of Web-based, customized enterprise software solution that integrated business process for scheduling employees.&lt;/p&gt;
+        &lt;h4&gt;Coursework to include:&lt;/h4&gt;
+        &lt;ul&gt;
+          &lt;li&gt;PHP 7&lt;/li&gt;
+          &lt;li&gt;MySQL&lt;/li&gt;
+          &lt;li&gt;JavaScript&lt;/li&gt;
+          &lt;li&gt;Enterprise Application Development&lt;/li&gt;
+          &lt;li&gt;Security&lt;/li&gt;
+          &lt;li&gt;It Project Management&lt;/li&gt;
+          &lt;li&gt;Web Design&lt;/li&gt;
+          &lt;li&gt;Computer Architecture&lt;/li&gt;
+          &lt;li&gt;Object Oriented Programming&lt;/li&gt;
+          &lt;li&gt;Bootstrap 3&lt;/li&gt;
+          &lt;li&gt;CSS 3&lt;/li&gt;
+        &lt;/ul&gt;
+        &lt;p&gt;&lt;a href="#" class="btn btn-primary" role="button"&gt;Capstone Project&lt;/a&gt; &lt;a href="http://stemuluscenter.org/deep-dive-coding-bootcamp-application/" class="btn btn-default" role="button"&gt;Apply For Deep Dive&lt;/a&gt;
+        &lt;/p&gt;
+      &lt;/div&gt;
     &lt;/div&gt;
-  &lt;/div&gt;
-  &lt;div class="col-md-3"&gt;
-    &lt;div class="content-box"&gt;
-      &lt;h4&gt;Kitty Ipsum&lt;/h4&gt;
-      &lt;p&gt;Burrow under covers jump off balcony, onto stranger's head nap all day meowing non stop for food spread kitty litter all over house. Purr for no reason meow so refuse to drink water except out of someone's glass. Leave dead animals as gifts jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed burrow under covers lay on arms while you're using the keyboard, cat is love, cat is life yet unwrap toilet paper.&lt;/p&gt;
+  &lt;/div&gt;&lt;!--/.col--&gt;
+
+  &lt;div class="col-sm-6 col-md-4"&gt;
+    &lt;div class="thumbnail"&gt;
+      &lt;img src="images/gdi.jpg" alt="Girl Develop It Logo" height="700" width="700"&gt;
+      &lt;div class="caption"&gt;
+        &lt;h3&gt;Girl Develop It!&lt;/h3&gt;
+        &lt;h3&gt;Albuquerque, NM&lt;/h3&gt;
+        &lt;h4&gt;Coursework to include:&lt;/h4&gt;
+        &lt;ul&gt;
+          &lt;li&gt;HTML5&lt;/li&gt;
+          &lt;li&gt;CSS 3&lt;/li&gt;
+          &lt;li&gt;WAMP&lt;/li&gt;
+          &lt;li&gt;PHP&lt;/li&gt;
+          &lt;li&gt;MySQL&lt;/li&gt;
+        &lt;/ul&gt;
+        &lt;p&gt;&lt;a href="https://www.girldevelopit.com/chapters/albuquerque" class="btn btn-primary" role="button"&gt;Girl Develop It!&lt;/a&gt; &lt;a href="http://www.meetup.com/Girl-Develop-It-Albuquerque/" class="btn btn-default" role="button"&gt;Meetup for Girl Develop It!&lt;/a&gt;
+        &lt;/p&gt;
+      &lt;/div&gt;
     &lt;/div&gt;
-  &lt;/div&gt;
-  &lt;div class="col-md-3"&gt;
-    &lt;div class="content-box"&gt;
-      &lt;h4&gt;Hipster Ipsum&lt;/h4&gt;
-      &lt;p&gt;Messenger bag food truck brooklyn mumblecore williamsburg, twee blue bottle banjo microdosing chicharrones dreamcatcher church-key hella cliche. Lumbersexual deep v single-origin coffee shabby chic intelligentsia. Mlkshk lo-fi try-hard, tote bag swag seitan celiac tacos.&lt;/p&gt;
-      &lt;p&gt;iPhone humblebrag crucifix helvetica organic direct trade pickled. IPhone blog small batch, art party post-ironic direct trade keytar butcher. Gluten-free post-ironic chia, tilde deep v brunch ennui shabby chic mumblecore green juice ethical sartorial distillery slow-carb helvetica.&lt;/p&gt;
+  &lt;/div&gt;&lt;!--.col--&gt;
+  ​
+  &lt;div class="col-sm-6 col-md-4"&gt;
+    &lt;div class="thumbnail"&gt;
+      &lt;img src="images/teamworks.png" alt="Team Works Logo" height="120" width="200"&gt;
+      &lt;div class="caption"&gt;
+        &lt;h3&gt;NM Commission on the Status of Women, Team Works&lt;/h3&gt;
+        &lt;h3&gt;Albuquerque, NM&lt;/h3&gt;
+        &lt;h4&gt;Coursework to include:&lt;/h4&gt;
+        &lt;ul&gt;
+          &lt;li&gt;Computer science&lt;/li&gt;
+          &lt;li&gt;user end information technology&lt;/li&gt;
+          &lt;li&gt;interpersonal skills&lt;/li&gt;
+          &lt;li&gt;Job readiness&lt;/li&gt;
+          &lt;li&gt;Intro to building websites&lt;/li&gt;
+        &lt;/ul&gt;
+      &lt;/div&gt;
     &lt;/div&gt;
-  &lt;/div&gt;
-  &lt;div class="col-md-3"&gt;
-    &lt;div class="content-box"&gt;
-      &lt;h4&gt;Office Ipsum&lt;/h4&gt;
-      &lt;p&gt;Value prop overcome key issues to meet key milestones, yet dogpile that. Social currency show pony bells and whistles pipeline can you ballpark the cost per unit for me deliverables. Reach out drill down, nor come up with something buzzworthy, but market-facing. Paddle on both sides level the playing field, and cross sabers. Accountable talk we need to button up our approach, pushback. Come up with something buzzworthy meeting assassin, but tbrand terrorists, but obviously. Old boys club get buy-in race without a finish line, three-martini lunch win-win cannibalize. Game-plan sacred cow. Paddle on both sides level the playing field, and cross sabers.&lt;/p&gt;
-    &lt;/div&gt;
-  &lt;/div&gt;
+  &lt;/div&gt;&lt;!--/.col--&gt;
+&lt;/div&gt;&lt;!--/.row--&gt;
+
+&lt;p class="pull-right"&gt;&lt;a href="#"&gt;Back to top&lt;/a&gt;&lt;/p&gt;
+
 &lt;/div&gt;&lt;!--.row--&gt;
 &lt;!-- End Column Example --&gt;
 				</pre><!--end source coe snippets-->
