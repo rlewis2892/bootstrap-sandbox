3b7d1700df077a976ad6ea17bee1e538afe69ea2
Rochelle Lewis <rlewis37@gmail.com>
2017-08-23 10:19:57 -0600

    create dhicks18 branch for scrollspy demo

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<?php require_once("lib/head-utils.html"); ?>
-		<title>Bootstrap Sandbox</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS Goes HERE -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Bootstrap Sandbox | Scrollspy</title>
 	</head>
 	<body>
 
-		<!--header include-->
-		<?php require_once("lib/header.html"); ?>
+		<header>
+			header here
+		</header>
 
 		<main>
 			<div class="container">
@@ -15,8 +43,9 @@
 			</div>
 		</main>
 
-		<!--footer include-->
-		<?php require_once("lib/footer.html"); ?>
+		<footer>
+			footer here
+		</footer>
 
 	</body>
 </html>
